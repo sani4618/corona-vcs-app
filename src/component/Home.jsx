f137ca9d1fe1136ed183cdde5d32da7d4aad3ee5
sani4618 <sanijoseph2018@gmail.com>
2023-08-10 11:21:20 +0530

    Home page is completed

---
+++ src/component/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.un.org/sites/un2.un.org/files/field/image/1583952355.1997.jpg" class="d-block w-100" alt="..." height="600"/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.livemint.com/lm-img/img/2023/04/20/1600x900/covid-new_1681695643779_1681963186911.jpg" class="d-block w-100" alt="..." height="600" />
+    </div>
+    <div class="carousel-item">
+      <img src="https://akm-img-a-in.tosshub.com/aajtak/images/author/corona-virus.jpg?size=1200:675" class="d-block w-100" alt="..." height="600"/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
